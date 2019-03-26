aefe7230c9471d9294daf9e644346ef87ed385de
David Mix F <David Mix F@10.32.168.60>
2019-03-26 13:41:14 -0300

    INSERT Arreglado

--- src/Modelo/ClienteModel.java
+++ src/Modelo/ClienteModel.java
@@ -9,10 +9,10 @@ public class ClienteModel extends Database{
     
     public ClienteModel(){}
 
+    //parametros: 1,2,3,4,5,6,7,8
     public  void InsertarCliente(String d_RUT, String d_DV, String d_NOMBRES, String d_APELLIDOP, String d_APELLIDOM, String d_SEXO, String d_NACIONALIDAD, Date d_FECHANAC) {
         try {
-            
-         String query = "INSERT INTO CLIENTE (RUT_CLIENTE,VERIF,NOMBRES,AP_PATERNO,AP_MATERNO,SEXO,FECHA_NAC,NACIONALIDAD) VALUES ('"+d_RUT+"','"+d_DV+"','"+d_NOMBRES+"','"+d_APELLIDOP+"','"+d_APELLIDOM+"','"+d_SEXO+"','"+d_NACIONALIDAD+"','"+d_FECHANAC+"')";
+         String query = "INSERT INTO CLIENTE (RUT_CLIENTE,VERIF,NOMBRES,AP_PATERNO,AP_MATERNO,SEXO,NACIONALIDAD,FECHA_NAC) VALUES ('"+d_RUT+"','"+d_DV+"','"+d_NOMBRES+"','"+d_APELLIDOP+"','"+d_APELLIDOM+"','"+d_SEXO+"','"+d_NACIONALIDAD+"','"+d_FECHANAC+"')";
          PreparedStatement pstm = this .getConexion().prepareStatement(query);
          pstm.executeUpdate();
         } catch (SQLException e) {
